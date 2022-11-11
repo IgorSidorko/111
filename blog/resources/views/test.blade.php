c9d6ac52f617233b831521dc7eb2884ee7b8356d
IgorSidorko <sidorko.im@gmail.com>
2022-11-11 15:16:00 +0600

    Создание AboutController и View test

---
+++ blog/resources/views/test.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+
+	<title>Праздник в каждый дом</title>
+</head>
+<body>
+<!---------------------------------------- Меню навигации ---------------------------------------->
+	<div class="header">
+		<nav>
+			<img src="">
+			<a href="">О нас</a>
+			<a href="">Каталог</a>
+			<a href="">Где нас найти?</a>
+		</nav>
+	</div>
+
+</body>
+</html>
