7957842b4cb1d1ca136474683bd7c34326a26606
IgorSidorko <sidorko.im@gmail.com>
2022-11-18 00:33:27 -0800

    Редактирование web и создание ShopController

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +12,3 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
